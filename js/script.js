1900102c42f419d1e99c87659fb12065839879d6
BiagioCaputo <scebiagio98@gmail.com>
2024-01-11 16:27:54 +0100

    Milestone 2 (funzione per creare numeri random)

--- js/script.js
+++ js/script.js
@@ -14,11 +14,23 @@ const startGame = event => {
     /* *********Funzioni interne al gioco********** */
     
     //funzione per creare una cella
-    const createCell = (number, cols) => {
+    const createCell = (number) => {
         const newCell = document.createElement('div');
         newCell.className = 'cell'; 
         newCell.innerText = number; // stampo il numero all'interno
         return newCell;
+    }
+
+    const createMines = (maxNumberMines) => {
+        const mines = [];
+        
+        while(mines.length < maxNumberMines){
+
+            const randomNumber = Math.floor(Math.random() * totalCells) +1;
+            if(!mines.includes(randomNumber)) mines.push(randomNumber);
+        }
+
+        return mines;
     }
     
     
@@ -29,13 +41,10 @@ const startGame = event => {
     button.innerText = "Ricomincia";
 
     //Svuoto la griglia 
-    grid.innerText = " ";
+    grid.innerText = "";
 
     //recupero il valore della tendina
-
     const level = dimension.value;
-
-    grid.classList.add(level);
 
     
     let rows= 10;
@@ -43,6 +52,10 @@ const startGame = event => {
     let cols = 10;
 
     let score = 0;
+
+    const maxNumberMines = 16;
+
+    
 
     switch(level){
         case 'hard':
@@ -57,36 +70,47 @@ const startGame = event => {
         break;
     }
 
-     //calcolo il totale delle celle
-     const totalCells = parseInt(rows) * parseInt(cols);
-     console.log(totalCells);
+    //assegno la grandezza alla griglia
+    grid.classList.add(level);
+
+
+    //calcolo il totale delle celle
+     const totalCells = rows * cols;
+
+    //creo le mie mine muahahahha
+    console.log(createMines (maxNumberMines));
         
 
-        //creo un ciclo for per ripetere la funzione tante volte quanto il totale delle celle indicato dall'utente
+    //creo un ciclo for per ripetere la funzione tante volte quanto il totale delle celle indicato dall'utente
     for(let i = 1; i <= totalCells; i++){
             
-            //creo una nuova cella con la i che corrisponde al numero ordinato
+        //creo una nuova cella con la i che corrisponde al numero ordinato
         const cell = createCell(i);
+        
 
-            //creo l'interazione al click
+        //creo l'interazione al click
         cell.addEventListener('click', () => {
             
             //creo un if per tornare indietro nel caso la casella sia già cliccata
             if(cell.classList.contains('clicked')) return;
+
             //aggiungo la classe clicked
             cell.classList.add('clicked');
             console.log(i);
+
             //incremento il punteggio e lo stampo in pagina
             Shownscore.innerText = ++score;
-            }
-            );
+        })
 
-            //aggiungo la nuova cella alla griglia
+        //aggiungo la nuova cella alla griglia
         grid.appendChild(cell);
-        }
-
-
     }
+
+
+}
 
 //metto in ascolto il form
 form.addEventListener('submit', startGame);
+
+//Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
+//Generiamoli e stampiamo in console per essere certi che siano corretti
